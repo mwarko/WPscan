2bf785a589a47b095e07451a46c6a33c965f8b94
mwarko <marcantoinefournier@bettersec.fr>
2015-08-24 08:48:24 +0200

    gui
    gui

--- wpscanner.py
+++ wpscanner.py
@@ -1,28 +1,45 @@
 import urllib.request
 import sys
 import os
+import tkinter
 
-user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-headers={'User-Agent':user_agent,} 
+fenetre = tkinter.Tk()
+fenetre.title("WPscanner By Marc-Antoine FOURNIER")
 
-chaine = "wordpress"
-adresse = str(input("Nom du site: "))
-request = urllib.request.Request('http://' + adresse,None,headers)
-response = urllib.request.urlopen(request)
-content = response.read()
-fichier = open("source.txt","a")
-fichier.writelines(str(content).lower())
-fichier.close()
-fichier = open("source.txt","r")
-for string in fichier:
-	if chaine in string:
-		print("Ce site est probablement fait avec wordpress")
-		break
-	else:
-		print("Ce site n'est probablement pas un site wordpress")
-fichier.close()
-os.remove("source.txt")
+def scanwp(adresse):
+	user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+	headers={'User-Agent':user_agent,} 
+	chaine = "wordpress"
+	request = urllib.request.Request('http://' + adresse,None,headers)
+	response = urllib.request.urlopen(request)
+	content = response.read()
+	fichier = open("source.txt","a")
+	fichier.writelines(str(content).lower())
+	fichier.close()
+	fichier = open("source.txt","r")
+	for string in fichier:
+		if chaine in string:
+			lbl.configure(text="Ce site est probablement fait avec wordpress")
+			break
+		else:
+			lbl.configure(text="Ce site n'est probablement pas un site wordpress")
+	fichier.close()
+	os.remove("source.txt")
 
-quit = str(input("Tapez sur une touche pour quitter..."))
 
+def main():
+	addr = texte.get()
+	scanwp(addr)
+
+
+def exxit():
+	exit()
+
+lbl = tkinter.Label(window, text="Rentrez le nom du site ici (exemple : google.com)")
+texte = tkinter.Entry(window)
+btn = tkinter.Button(window, text="OK", command=main)
+btnquit = tkinter.Button(window, text="Quitter", command=exxit)
+lbl.pack()
+btn.pack()
+fenetre.pack()
 
